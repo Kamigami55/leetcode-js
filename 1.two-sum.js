1387c31b707a8508aaa4ffe762fbdaf489342695
Kamigami55 <eason@easonchang.com>
2021-11-23 23:36:07 +0800

    Add 1-two-sum

---
+++ 1.two-sum.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (map[diff] !== undefined) {
+      return [map[diff], i];
+    }
+    map[nums[i]] = i;
+  }
+};
+// @lc code=end
+
