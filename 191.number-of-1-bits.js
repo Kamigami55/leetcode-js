6da6dcdc8c7fb4d9a70727ffea83bf7d31a66d8b
Kamigami55 <eason@easonchang.com>
2022-01-21 20:34:55 +0800

    Update [191] Number of 1 Bits

--- 191.number-of-1-bits.js
+++ 191.number-of-1-bits.js
@@ -14,11 +14,26 @@
 Your runtime beats 31.98 % of javascript submissions
 Your memory usage beats 98.1 % of javascript submissions (39.7 MB)
 */
+// var hammingWeight = function (n) {
+//   let ans = 0
+//   while (n !== 0) {
+//     if (n & 1) ++ans
+//     n = n >>> 1
+//   }
+//   return ans
+// }
+
+/*
+Faster solution: bit trick
+601/601 cases passed (97 ms)
+Your runtime beats 41.48 % of javascript submissions
+Your memory usage beats 85.81 % of javascript submissions (40 MB)
+*/
 var hammingWeight = function (n) {
   let ans = 0
   while (n !== 0) {
-    if (n & 1) ++ans
-    n = n >>> 1
+    ++ans
+    n = n & (n - 1) // reset the least significant bit (right most 1) to 0
   }
   return ans
 }
